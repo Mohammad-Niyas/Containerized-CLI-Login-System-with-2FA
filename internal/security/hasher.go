b9ca3e92d63c4439ac07b844fc2cb64ab46b978f
Mohammad-Niyas <mohammadniyas662@gmail.com>
2026-09-10 20:50:23 +0000

    implement bcrypt hasher, totp service, and csprng token generator

--- internal/security/hasher.go
+++ internal/security/hasher.go
@@ -1,1 +1,38 @@
 package security
+
+import (
+	"errors"
+
+	"github.com/Mohammad-Niyas/Containerized-CLI-Login-System-with-2FA/internal/domain"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type BcryptHasher struct {
+	cost int
+}
+
+func NewBcryptHasher(cost int) *BcryptHasher {
+	if cost <= 0 {
+		cost = bcrypt.DefaultCost
+	}
+	return &BcryptHasher{cost: cost}
+}
+
+func (h *BcryptHasher) Hash(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), h.cost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func (h *BcryptHasher) Compare(hashedPassword, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return domain.ErrInvalidCredentials
+		}
+		return err
+	}
+	return nil
+}
